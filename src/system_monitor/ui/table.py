6b2496b848b75acaeceb5df6f971f19370b4e3bf
Caio Ciriaco <caiocrcontas@gmail.com>
2025-12-19 10:54:30 -0300

    chore: add colors to current temp

--- src/system_monitor/ui/table.py
+++ src/system_monitor/ui/table.py
@@ -2,14 +2,29 @@ from rich.table import Table
 from rich.panel import Panel
 from rich.align import Align
 from rich.console import Group
+from rich.text import Text
 from rich import box
 
-def fmt_metric(metric: float | None, unit: str) -> str:
-    if metric is None:
-        return "?"
-    return f"{metric:6.2f}{unit}"
+def fmt_metric(metric: float | None, unit: str, current: bool = False) -> Text:
+    color = ""
 
-def build_table(cpu, nvme, ram, gpu) -> Group:
+    if not metric == None and current:
+        if metric > 80:
+            color = "red"
+        elif metric > 50:
+            color = "yellow"
+        else:
+            color = "green"
+
+    if metric is None:
+        return Text("?")
+
+    text = Text(f"{metric:6.2f}{unit}")
+    text.stylize(color)
+
+    return text
+
+def build_table(cpu, storage, ram, gpu) -> Group:
     table = Table(box=box.ASCII2, expand=False)
 
     table.add_column("Sensor", width=40, no_wrap=True)
@@ -17,40 +32,43 @@ def build_table(cpu, nvme, ram, gpu) -> Group:
     table.add_column("Min", width=15, justify="right")
     table.add_column("Max", width=15, justify="right")
 
-    # CPU
     table.add_row(cpu.name)
+    table.add_row("|- Temperatures")
     table.add_row(
-        f"└─ Temp ({cpu.sensor})",
-        fmt_metric(cpu.temp.current, cpu.temp.unit),
+        f"   |- {cpu.sensor}",
+        fmt_metric(cpu.temp.current, cpu.temp.unit, True),
         fmt_metric(cpu.temp.min, cpu.temp.unit),
         fmt_metric(cpu.temp.max, cpu.temp.unit),
     )
 
-    table.add_row("   Clocks")
+    table.add_row()
+
+    table.add_row("|- Clocks")
     for i, clock in enumerate(cpu.clocks):
         table.add_row(
-            f"   └─ {clock.name}" if i == 0 else f"      {clock.name}",
+            f"   |- {clock.name}",
             fmt_metric(clock.current, f" {clock.unit}"),
             fmt_metric(clock.min, f" {clock.unit}"),
             fmt_metric(clock.max, f" {clock.unit}"),
         )
 
-    for i, dev in enumerate(nvme.devices):
-        table.add_row()
-        table.add_row(f"NVME #{i} - {nvme.devices[i].sensor}")
+    table.add_row()
+
+    for i, dev in enumerate(storage.devices):
+        table.add_row(f"{dev.model} ({dev.name})")
         table.add_row(
-            "└─ Temp",
-            fmt_metric(dev.temp.current, dev.temp.unit),
+            "|- Temperature",
+            fmt_metric(dev.temp.current, dev.temp.unit, True),
             fmt_metric(dev.temp.min, dev.temp.unit),
             fmt_metric(dev.temp.max, dev.temp.unit),
         )
 
     for i, dev in enumerate(ram.devices):
         table.add_row()
-        table.add_row(f"DIMM #{i} - {ram.devices[i].sensor}")
+        table.add_row(f"DIMM #{i}")
         table.add_row(
-            "└─ Temp",
-            fmt_metric(dev.temp.current, dev.temp.unit),
+            "|- Temperature",
+            fmt_metric(dev.temp.current, dev.temp.unit, True),
             fmt_metric(dev.temp.min, dev.temp.unit),
             fmt_metric(dev.temp.max, dev.temp.unit),
         )
@@ -60,8 +78,8 @@ def build_table(cpu, nvme, ram, gpu) -> Group:
     if gpu.temp is not None:
         table.add_row(gpu.name)
         table.add_row(
-            "└─ Temp",
-            fmt_metric(gpu.temp.current, gpu.temp.unit),
+            "|- Temperature",
+            fmt_metric(gpu.temp.current, gpu.temp.unit, True),
             fmt_metric(gpu.temp.min, gpu.temp.unit),
             fmt_metric(gpu.temp.max, gpu.temp.unit),
         )
